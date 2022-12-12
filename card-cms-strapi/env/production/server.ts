8a5fc5903634209802bf040dce20effbd0209de1
Teeldinho <tshepangmog10@gmail.com>
2022-12-12 21:41:36 +0200

    Added config files for deployment

--- card-cms-strapi/env/production/server.ts
+++ card-cms-strapi/env/production/server.ts
@@ -1,6 +1,6 @@
-// path: ./config/env/production/server.js
+// path: ./config/env/production/server.ts
 
-module.exports = ({ env }) => ({
+export default ({ env }) => ({
   proxy: true,
   url: env("APP_URL"), // replaces `host` and `port` properties in the development environment
   app: {
