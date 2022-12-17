a7a0f6c59b8be4368c792df3315c40f4431c8f97
Teeldinho <tshepangmog10@gmail.com>
2022-12-17 12:30:08 +0200

    Removed overflow on large screens

--- components/PageDecoration.tsx
+++ components/PageDecoration.tsx
@@ -6,11 +6,11 @@ const PageDecoration = () => {
       {/* DESKTOP DECORATION */}
 
       {/* top right decoration */}
-      <div className="absolute -top-72 -right-48 -z-10 hidden h-[915px] w-[915px] rounded-full bg-white bg-gradient-decorator-circle blur-2xl lg:block"></div>
+      <div className="absolute -top-72 -right-48 -z-10 hidden h-[50rem] w-[50rem] rounded-full bg-white bg-gradient-decorator-circle blur-2xl lg:block"></div>
       {/* top left decoration */}
-      <div className="absolute -top-40 -left-24 -z-10 hidden h-[637px] w-[637px] rounded-full bg-white bg-gradient-decorator-circle blur-2xl lg:block"></div>
+      <div className="absolute -top-40 -left-24 -z-10 hidden h-[40rem] w-[40rem] rounded-full bg-white bg-gradient-decorator-circle blur-2xl lg:block"></div>
       {/* bottom right decoration */}
-      <div className="absolute top-[688px] -right-48 -z-10 hidden h-[664px] w-[664px] rounded-full bg-white bg-gradient-decorator-circle blur-2xl lg:block"></div>
+      <div className="absolute top-[688px] -right-48 -z-10 hidden h-[45rem] w-[45rem] rounded-full bg-white bg-gradient-decorator-circle blur-2xl lg:block"></div>
 
       {/* MOBILE DECORATION */}
       {/* left decoration */}
